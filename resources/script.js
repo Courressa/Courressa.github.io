36799e6cf4fe0600b9a2e51ba14a63bc50d62f16
Courressa <courressamalcolm2030@gmail.com>
2024-07-07 00:04:10 -0400

    add butterfly movement for game

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,11 @@
 let arrow = document.getElementById('down-arrow');
 let game = document.getElementById('secret-game');
+let butterflyAndBubble = document.getElementById('butterfly-ingame');
 
 //reveals the "Secret" Game section
 const reveal = () => {
     if (game.style.display == '' || game.style.display == 'none') {
-        game.style.display = 'block';
+        game.style.display = 'flex';
     } else {
         game.style.display = 'none';
     }
@@ -12,3 +13,30 @@ const reveal = () => {
 }
 
 arrow.addEventListener('click', reveal);
+
+//moving butterfly
+
+let initialPosition = 48;
+
+let stopAtEdge = () => {
+    do {
+        initialPosition--
+    } while (initialPosition > 0);
+};
+
+const movement = (press) => {
+    stopAtEdge();
+        switch (press.code) {
+            case "ArrowLeft":
+            case "KeyA":
+                butterflyAndBubble.style.left = `${initialPosition--}%`;
+                break;
+            case "ArrowRight":
+            case "KeyD":
+                butterflyAndBubble.style.left = `${initialPosition++}%`;
+                break;
+        }
+};
+
+
+document.addEventListener('keydown', movement);
